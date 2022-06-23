c86bcd789a10b0b5a231bd9fb01386315f2f1755
Alex Shevlin <alexshevlin1@gmail.com>
2022-06-23 11:45:30 -0700

    add vowelCount function.
    Co-authored-by: ryandefea <ryandefea@users.noreply.github.com>

--- src/js/haiku.js
+++ src/js/haiku.js
@@ -3,6 +3,13 @@ import Poem from './poem.js';
 export default class Haiku extends Poem {
   constructor(line) {
     super('haiku');
+    //haiku is type 'haiku' for Poem.type
     this.line = line;
+  }
+  vowelCount(line) {
+    let vowelCount = line.match(/[aeiouy]{1,2}/ig).length;
+    return vowelCount;
   }    
 }
+
+
