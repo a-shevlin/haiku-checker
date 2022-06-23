a4595b2b42066c39ed20358b3d91abdfefe4c34b
Alex Shevlin <alexshevlin1@gmail.com>
2022-06-23 11:44:37 -0700

    add vowelCount function to Haiku and pass test.
    Co-authored-by: ryandefea <ryandefea@users.noreply.github.com>

--- __tests__/haiku.test.js
+++ __tests__/haiku.test.js
@@ -5,4 +5,9 @@ describe ('Haiku', () => {
     let newHaiku = new Haiku('line');
     expect(newHaiku.line).toBe('line');
     });
+  test('should return vowel count from line', () => {
+    let newVowel = new Haiku('how many vowels');
+    let vowelAmount = newVowel.vowelCount(newVowel.line);
+    expect(vowelAmount).toEqual(5);
+  });  
 });
