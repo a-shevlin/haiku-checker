3f7747eb2102497bfe9d3d5396cc57b6db5b8be6
Alex Shevlin <alexshevlin1@gmail.com>
2022-06-23 16:18:14 -0700

    update index.js  for multiple line output to haikuLine.
    Co-authored-by: ryandefea <ryandefea@users.noreply.github.com>

--- src/index.js
+++ src/index.js
@@ -11,21 +11,21 @@ let lineInput3 = $('#textInput').val();
 let newHaiku = new Haiku(lineInput1, lineInput2, lineInput3);
 let passLine1 = newHaiku.lineCount5(newHaiku.line1);
 if (passLine1 = true) {
-  $(".haiku").append(newHaiku.line1);
+  $(".haikuLine1").append(newHaiku.line1);
 } else {
-  $(".haiku").text('this line does not fit haiku 5-7-5');
+  $(".haikuLine1").text('this line does not fit haiku 5-7-5');
 }
 let passLine2 = newHaiku.lineCount7(newHaiku.line2);
 if (passLine2 = true) {
-  $(".haiku").append(newHaiku.line2);
+  $(".haikuLine2").append(newHaiku.line2);
 } else {
-  $(".haiku").text('this line does not fit haiku 5-7-5');
+  $(".haikuLine2").text('this line does not fit haiku 5-7-5');
 }
 let passLine3 = newHaiku.lineCount5(newHaiku.line3);
 if (passLine3 = true) {
-  $(".haiku").append(newHaiku.line3);
+  $(".haikuLine3").append(newHaiku.line3);
 } else {
-  $(".haiku").text('this line does not fit haiku 5-7-5');
+  $(".haikuLine3").text('this line does not fit haiku 5-7-5');
 }
 console.log(passLine);
 //true
